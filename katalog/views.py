8d280dd12844a6100f0a6f9089d7ee6156e4a45c
Dhafin Raditya Juliawan <dhafinraditya35@gmail.com>
2022-09-11 11:26:19 +0700

    Finished routing and connecting with database

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog (request):
+    data_catalog = CatalogItem.objects.all()
+    context = {
+        'list_catalog': data_catalog,
+        'name': "Dhafin R. Juliawan",
+        'student_id': "2106650304"
+    }
+    return render(request, "katalog.html", context)
