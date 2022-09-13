b4b95a99cd9ed74d9d39a30cf0da05cb29e08b11
Dhafin Raditya Juliawan <dhafinraditya35@gmail.com>
2022-09-13 17:22:32 +0700

    Adding views testing

---
+++ katalog/tests/test_views.py
@@ -0,0 +1,16 @@
+from django.test import TestCase, Client
+from django.urls import reverse
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.katalog_url = reverse('katalog:katalog')
+
+    def test_catalog_get(self):
+        response = self.client.get(self.katalog_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'katalog/katalog.html')
+
+# Reference
+# https://www.youtube.com/watch?v=hA_VxnxCHbo
