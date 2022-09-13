e2876b2d99fa7dd15de1527e57081fe89d761bfd
Dhafin Raditya Juliawan <dhafinraditya35@gmail.com>
2022-09-13 17:38:32 +0700

    Adding models test

--- katalog/tests/test_models.py
+++ katalog/tests/test_models.py
@@ -1,2 +1,19 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
+from django.test import TestCase
+from katalog.models import CatalogItem
+
+class TestModels(TestCase):
+    def setUp(self):
+        self.catalogTest = CatalogItem.objects.create(
+            item_name = 'Kering Kentang',
+            item_price = 30000,
+            item_stock = 15,
+            description = 'Cemilan olahan kentang yang asin dan pedas',
+            rating = 10,
+            item_url = 'https://www.tokopedia.com/tatasnacks/kering-kentang-mustofa-original'
+        )
+
+    def test_catalog_item_dummy(self):
+        self.assertEqual(
+            self.catalogTest, 
+            CatalogItem.objects.get(item_name = 'Kering Kentang')
+        )
