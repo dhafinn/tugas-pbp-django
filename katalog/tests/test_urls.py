a5db46858c61836c2491ff75f38cc586e501b306
Dhafin Raditya Juliawan <dhafinraditya35@gmail.com>
2022-09-13 17:44:03 +0700

    Finished urls testing

--- katalog/tests/test_urls.py
+++ katalog/tests/test_urls.py
@@ -7,4 +7,7 @@ class UrlsTest(SimpleTestCase):
     def test_url(self):
         url = reverse('katalog:katalog')
         self.assertEquals(resolve(url).func, show_catalog)
-        
+
+
+# Reference
+# https://www.youtube.com/watch?v=0MrgsYswT1c&list=PLbpAWbHbi5rMF2j5n6imm0enrSD9eQUaM&index=2    
